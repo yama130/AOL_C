b16b12fc845817709ed62385a2f770950bf39c6a
yama130 <ouos@hotmail.com>
2015-06-21 10:13:58 +0900

    1st save

--- ALDS1/ALDS1_1_C.c
+++ ALDS1/ALDS1_1_C.c
@@ -1,28 +1,34 @@
 #include<stdio.h>
-int main(){
-  int x , y,a,b,min,gch;
-  scanf("%d %d", &x, &y);
-  while (1) {
-    a=minCh(x,y);
-    b=gcdCh(x,y);
-    // printf("%d\n",a);
-    // printf("%d\n",b);
+#include <math.h>
 
-    if(a==b || b==0){
-      break;
-    }else{x=a;y=b;}
+int main(){
+  int n,a=0;
+  long A[10000];
+  scanf("%d", &n);
+  for(int i=0;i<n;i++){
+    scanf("%ld", &A[i+1]);
+    a=a+isPlime(A[i+1]);
   }
 
   printf("%d\n",a);
   return 0;
 }
-int minCh(int a,int b){
+int isPlime(long a){
 	int x;
-	if(a>b){x=b;}else{x=a;}
-	return x;
-}
-int gcdCh(int a,int b){
-	int x;
-	if(a>b){x=a%b;}else{x=b%a;}
+  if(a==2){x=1;}
+  else if(a==3){x=1;}
+  else if(a==4){x=0;}
+  else if(a==5){x=1;}
+  else if(a%2==0){x=0;}
+  else{
+
+    for(int i=3;i<(int)sqrt((double)a);i++){
+      if(a%i==0){
+        x=0;
+        printf("sq:%d %d\n",a%i,i);
+      }else{x=0;}
+    }
+    x=0;
+    }
 	return x;
 }
