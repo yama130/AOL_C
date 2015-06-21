0a27185b036ab1033300506901b4a522621b6252
yama130 <ouos@hotmail.com>
2015-06-21 12:33:20 +0900

    for(int i=...)はJAVAだけCのAOLバージョンはコンパイルエラーが発生

--- ALDS1/ALDS1_1_C.c
+++ ALDS1/ALDS1_1_C.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
 #include <math.h>
 
-int main(){
-  int n,m=0;
+main(){
+  int i,n,m=0;
   long A[10000];
   scanf("%d", &n);
-  for(int i=0;i<n;i++){
+  for(i=0;i<n;i++){
     scanf("%ld", &A[i+1]);
-    //printf("set1:%d\n", A[i+1]);
-    //printf("set2:%d %lf\n", A[i+1],sqrt((double)(A[i+1])));
-    //printf("set2:%d %d\n", A[i+1],(int)(sqrt((double)(A[i+1]))));
-    //printf("set3:%d\n", isPlime(A[i+1]));
-
     m=m+isPlime(A[i+1]);
   }
 
