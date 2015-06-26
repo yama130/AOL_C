692d9f8542da337994df72c2790447614498b568
yama130 <ouos@hotmail.com>
2015-06-27 02:08:28 +0900

    complete

--- Vol01/0101.c
+++ Vol01/0101.c
@@ -2,18 +2,15 @@
 #include<string.h>
 int main(){
   int n,i;
-  char str[1000][1028];
+  char str1[1028];
+  char *str2;
   scanf("%d\n", &n);
   for(i=0;i<n;i++){
-    fgets(str[i],sizeof(str[i]),stdin);
-    //scanf("%s\n", &str[i]);
-    if(strstr(str[i],"Hoshino")!=0){
-      strncpy(strstr(str[i],"Hoshino"),"Hoshina",7);
+    gets(str1);
+    while ((str2 = strstr(str1, "Hoshino")) != NULL){
+      strncpy(strstr(str1,"Hoshino"),"Hoshina",7);
     }
-  }
-  //printf("start\n");
-  for(i=0;i<n;i++){
-    printf("%s", str[i]);
+    printf("%s\n", str1);
   }
     return 0;
 }
