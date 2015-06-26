3b070b3574ca46a8b04fd3cac155bf441956f686
yama130 <ouos@hotmail.com>
2015-06-27 02:41:53 +0900

    0100再登録

--- Vol01/0100.c
+++ Vol01/0100.c
@@ -2,50 +2,45 @@
 int main(){
 	int n, m, i, j, k;
 	long id; long tanka, count;
-	//int ID[4000],URI[4000];
 	long R_ID[4000];
 	m = 0;
 	scanf("%d", &n);
 	while (n != 0){
-		//get count
-		long ID[4000] = {0};
-		long URI[4000] = {0};
-		//get dataset
-		for (i = 0; i<n; i++){
-			scanf("%d %d %d", &id, &tanka, &count);
-			j = 0;
-			while (id != 0){
-				if (ID[j] == 0){
-					ID[j] = id; URI[j] = tanka*count;
-					break;
-				}
-				else if (ID[j] == id){
-					URI[j] = URI[j] + tanka*count;
-					break;
-				}
-				else j++;
-			}
-		}
-		//dataset chk
-		R_ID[m] = -1;
-		for (i = 0; i<n; i++){
-			if (URI[i] >= 1000000){ R_ID[m] = ID[i]; m++; }
-		}
-		if (R_ID[m] == -1){ m++; }
-		//get count
-		scanf("%d", &n);
+	//get count
+	long ID[4000] = {0};
+	long URI[4000] = {0};
+	//get dataset
+	for (i = 0; i<n; i++){
+	  scanf("%d %d %d", &id, &tanka, &count);
+	  j = 0;
+	  while (id != 0){
+	      if (ID[j] == 0){
+	          ID[j] = id; URI[j] = tanka*count;
+	          break;
+	      }
+	      else if (ID[j] == id){
+	          URI[j] = URI[j] + tanka*count;
+	          break;
+	      }
+	      else j++;
+	  }
 	}
-	//k=0;
-	//while(ID[k]!=0){
-	// printf("%d %d\n",ID[k],URI[k]);
-	// k++;
-	//}
+	//dataset chk
+	R_ID[m] = -1;
+	for (i = 0; i<n; i++){
+	  if (URI[i] >= 1000000){ R_ID[m] = ID[i]; m++; }
+	}
+	if (R_ID[m] == -1){ m++; }
+	//get count
+	scanf("%d", &n);
+	}
 	k = 0;
 	while (R_ID[k] != 0){
-		if (R_ID[k] == -1){ printf("NA\n"); }
-		else{ printf("%d\n", R_ID[k]); }
-		k++;
+	if (R_ID[k] == -1){ printf("NA\n"); }
+	else{ printf("%d\n", R_ID[k]); }
+	k++;
 	}
 
 	return 0;
+
 }
