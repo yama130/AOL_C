22bdd481702cd8319bc623d7ad19be94efa7e6d5
yama130 <ouos@hotmail.com>
2015-06-29 12:13:24 +0900

    add 0200.c

--- vol02/0200.c
+++ vol02/0200.c
@@ -3,14 +3,14 @@
 int main(){
 	int n,rt;
 	int frSt,toSt,coVal,tiVal;
-	int h,i,ti[100][100]={0},co[100][100]={0};
+	int h,i,ti[101][101]={0},co[101][101]={0};
 	while (1) {
 		//1stRecGet
 		scanf("%d %d", &n, &rt);
 		if(n==0 && rt==0)break;
 		//root-format
-		for(h=0;h<n;h++){
-			for(i=0;i<n;i++){
+		for(h=0;h<n+1;h++){
+			for(i=0;i<n+1;i++){
 				ti[h][i]=10000;co[h][i]=10000;
 		}}
 		//root-set1
@@ -24,25 +24,25 @@ int main(){
 		for(j=1;j<rt+1;j++){for(k=1;k<rt+1;k++){for(l=1;l<rt+1;l++){
 			if(co[j][l]>co[j][k]+co[k][l]){
 				co[j][l]=co[j][k]+co[k][l];
-				co[l][j]=co[j][k]+co[k][l];
-				// printf("cost:%d %d %d %d\n",j,k,l,co[j][l]);
-				}
+				co[l][j]=co[j][k]+co[k][l];}
+				// printf("cost:%d %d %d %d\n",j,k,l,co[j][l]);}
 			if(ti[j][l]>ti[j][k]+ti[k][l]){
-				ti[j][l]=ti[j][k]+ti[k][l	];
+				ti[j][l]=ti[j][k]+ti[k][l];
 				ti[l][j]=ti[j][k]+ti[k][l];}
 		}}}
-
-		int m;
+		// root-get
+		int m;//,j;
 		int stSt,glSt,chTyp;
-		//int j;
 		scanf("%d", &m);
 		for(j=0;j<m;j++){
 			scanf("%d %d %d", &stSt,&glSt,&chTyp);
 			//minCost
-			if(chTyp==0)printf("minCost:%d\n", co[stSt][glSt]);
+			// if(chTyp==0)printf("minCost:%d\n", co[stSt][glSt]);
+			if(chTyp==0)printf("%d\n", co[stSt][glSt]);
 		  //minTime
-			if(chTyp==1)printf("minTime:%d\n", ti[stSt][glSt]);
-			}
+			// if(chTyp==1)printf("minTime:%d\n", ti[stSt][glSt]);
+			if(chTyp==1)printf("%d\n", ti[stSt][glSt]);
+		}
 	}
 	return 0;
 }
